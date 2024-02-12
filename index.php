6868d10f1345bfc5fb897673d98943309097732c
Luis Leguizamón <lleguizamonl@gmail.com>
2024-02-12 16:11:40 -0300

    Improvement: Replace text on buttons for icons

--- index.php
+++ index.php
@@ -41,6 +41,12 @@
             border: none;
             cursor: pointer;
         }
+
+        #prevBtn:hover,
+        #nextBtn:hover{
+            color: lightgray;
+            transition: color 0.3s ease-in-out;
+        }
     </style>
 </head>
 
@@ -52,8 +58,16 @@
             <img class="carousel-item" src="/image3.jpg" alt="">
         </div>
         <div class="carousel-buttons">
-            <button id="prevBtn">Prev</button>
-            <button id="nextBtn">Next</button>
+            <button id="prevBtn">
+                <svg class="preicon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left">
+                    <path d="m15 18-6-6 6-6" />
+                </svg>
+            </button>
+            <button id="nextBtn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
+                    <path d="m9 18 6-6-6-6" />
+                </svg>
+            </button>
         </div>
     </div>
 
